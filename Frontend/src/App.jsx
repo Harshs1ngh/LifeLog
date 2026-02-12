4a4d8b3817f57323f2538689ea5569a78830989c
Harshs1ngh <singharsh.cse@gmail.com>
2026-02-12 21:24:35 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./index.css";
 import axios from "axios";
-
 import Header from "./components/header";
 import Journal from "./components/journal"; 
 import Insights from "./components/insight";
