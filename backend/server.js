ee1e965d1095d67c040ced86c48f680db378f455
Harshs1ngh <singharsh.cse@gmail.com>
2026-04-15 12:54:49 +0530

    url updated

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://innerspace-iota.vercel.app/",  
+    "https://innerspace-iota.vercel.app",  
   ],
   credentials: true,
 }));
